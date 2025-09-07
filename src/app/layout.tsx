22c8369301192642191c4cefd61188daedc3833b
pok1220 <pokkung1220@gmail.com>
2025-09-07 20:03:04 +0700

    feat: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import NextAuthProvider from "../providers/NextAuthProvider";
 import { LoadingProvider } from "@/providers/LoadingProvider";
-
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -12,7 +13,7 @@ export default async function RootLayout({
   const nextAuthSession = await getServerSession(authOptions);
 
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body className="min-h-screen bg-[#F9F8F9] ">
         <NextAuthProvider session={nextAuthSession}>
           <LoadingProvider>{children}</LoadingProvider>
